68c64960a3399461d70b534dd12e794d778300e1
Nikolas Daynard <Nkdaynard@gmail.com>
2025-03-01 11:29:00 -0500

    make l4 not crash into end stop

--- src/main/java/frc/robot/constants/ElevatorConstants.java
+++ src/main/java/frc/robot/constants/ElevatorConstants.java
@@ -6,7 +6,7 @@ public class ElevatorConstants {
     public static final double elevatorGearRatio = 1.0 / 12.0; // 12:1 (12 mot rot = 1 rot)
     public static final double elevatorGearDiameterInches = 1.7453703703703705; // 
     public static final double elevatorGearCircumferenceInches = elevatorGearDiameterInches * Math.PI; // 2PIr = PId
-    public static final double elevatorConversionFactor = elevatorGearRatio * elevatorGearCircumferenceInches; // amps
+    public static final double elevatorConversionFactor = elevatorGearRatio * elevatorGearCircumferenceInches;
 
     public static final double kP = 0.2;
     public static final double kI = 0.001;
