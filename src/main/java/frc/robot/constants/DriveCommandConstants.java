a30f4006e28d1670194f3162b0b1c9b5468622ba
Nikolas Daynard <Nkdaynard@gmail.com>
2025-05-21 07:18:42 -0400

    tune align

--- src/main/java/frc/robot/constants/DriveCommandConstants.java
+++ src/main/java/frc/robot/constants/DriveCommandConstants.java
@@ -12,7 +12,7 @@ public class DriveCommandConstants {
     public static final double kMaxVelocity = 0;
     public static final double kMaxAccel = 0;
 
-    public static final double xGoal = .42; // .42 is aligned bumper
+    public static final double xGoal = .485; // .42 is aligned bumper
     public static final double x2Goal = xGoal + .5; // .42 is aligned bumper
     public static final double yGoal = 0;
     public static final double kThetaP = 0.5;
